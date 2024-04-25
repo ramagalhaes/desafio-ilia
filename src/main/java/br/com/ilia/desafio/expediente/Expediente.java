1e1a1c48f6540c6fba519d5e1cde347cd51415c5
Raphael Magalhães <raphael.gol@hotmail.com>
2024-04-25 12:57:21 -0300

    refact - business logic inside model

--- src/main/java/br/com/ilia/desafio/expediente/Expediente.java
+++ src/main/java/br/com/ilia/desafio/expediente/Expediente.java
@@ -1,8 +1,11 @@
 package br.com.ilia.desafio.expediente;
 
+import br.com.ilia.desafio.exceptions.BatidaExistsException;
+import br.com.ilia.desafio.exceptions.ExpedienteValidationException;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +27,33 @@ public class Expediente {
     private List<String> pontos = new ArrayList<>();
 
     public void addPonto(String ponto) {
-        this.pontos.add(ponto);
+        canRegisterNewPonto();
+        validateIfPontoAlreadyExists(ponto);
+        validateIfIsReturnFromLaunch(ponto);
+        pontos.add(ponto);
+    }
+
+    private void validateIfPontoAlreadyExists(String ponto) {
+        if (pontos.contains(ponto)) {
+            throw new BatidaExistsException("Horário já registrado");
+        }
+    }
+
+    private void validateIfIsReturnFromLaunch(String ponto) {
+        if (pontos.size() == 2) {
+            LocalTime saidaAlmoco = LocalTime.parse(pontos.get(1));
+            LocalTime voltaAlmoco = LocalTime.parse(ponto);
+            if (saidaAlmoco.plusHours(1).isAfter(voltaAlmoco)) {
+                throw new ExpedienteValidationException("Deve haver no mínimo 1 hora de almoço");
+            }
+
+        }
+    }
+
+    private void canRegisterNewPonto() {
+        if (pontos.size() == 4) {
+            throw new ExpedienteValidationException("Apenas 4 horários podem ser registrados por dia");
+        }
     }
 
 }
--- src/main/java/br/com/ilia/desafio/expediente/handlers/ExpedienteHandler.java
+++ src/main/java/br/com/ilia/desafio/expediente/handlers/ExpedienteHandler.java
@@ -1,12 +1,9 @@
 package br.com.ilia.desafio.expediente.handlers;
 
-import br.com.ilia.desafio.exceptions.BatidaExistsException;
-import br.com.ilia.desafio.exceptions.ExpedienteValidationException;
 import br.com.ilia.desafio.expediente.Batida;
 import br.com.ilia.desafio.expediente.Expediente;
 import br.com.ilia.desafio.expediente.ExpedienteRepository;
 
-import java.time.LocalTime;
 import java.util.Optional;
 
 public final class ExpedienteHandler extends BatidaHandler {
@@ -28,29 +25,7 @@ public final class ExpedienteHandler extends BatidaHandler {
 
         if (hasExpediente.isEmpty()) {
             expediente.setDia(dia);
-            expediente.addPonto(hora);
-            return expediente;
         }
-
-        Optional<String> existingPonto = expediente.getPontos().stream().filter(p -> p.equals(hora)).findFirst();
-        if (existingPonto.isPresent()) {
-            throw new BatidaExistsException("Horário já registrado");
-        }
-
-        if (expediente.getPontos().size() == 2) {
-            LocalTime saidaAlmoco = LocalTime.parse(expediente.getPontos().get(1));
-            LocalTime voltaAlmoco = LocalTime.parse(hora);
-
-            if (saidaAlmoco.plusHours(1).isAfter(voltaAlmoco)) {
-                throw new ExpedienteValidationException("Deve haver no mínimo 1 hora de almoço");
-            }
-
-        }
-
-        if (expediente.getPontos().size() == 4) {
-            throw new ExpedienteValidationException("Apenas 4 horários podem ser registrados por dia");
-        }
-
         expediente.addPonto(hora);
         return expediente;
     }
